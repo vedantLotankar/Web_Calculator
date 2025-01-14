b7e3dda1c6e87218e743de68ed5e0ec6b7f7076b
AtharvaD1407 <codingATHU69@gmail.com>
2025-01-14 22:20:34 +0530

    Round Error

--- web_cal.js
+++ web_cal.js
@@ -16,7 +16,7 @@ function calculate() {
         
         display.value = result;
         
-        display.value = Math.round(eval(display.value), 3);
+        display.value = eval(display.value);
       
         display.scrollLeft = display.scrollWidth;
     } catch (error) {
