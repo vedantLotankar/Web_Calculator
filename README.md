652a92172e92d2ed3d90ebe6ccbcc3983365cb58
vedantLotankar <lotankarvedant10@gmail.com>
2025-01-15 22:48:05 +0530

    Final js file code fix

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 This is a basic web calculator with basic math operations
-
-Things to improve :-
-1. text display window
-2. replace "*" with "x" in the display
-3. add a backspace option
-4. advancd functionalities 
--- web_cal.js
+++ web_cal.js
@@ -1,35 +1,27 @@
 const display = document.getElementById("display");
 
-
-function appendToDisplay(input){
-
 function appendToDisplay(input) {
     display.value += input;
 }
-
-
-function clearDisplay(){
-
 
 function clearDisplay() {
     display.value = "";
 }
 
-
-function calculate(){
+function calculate() {
     try {
-        display.value = Math.round(eval(display.value), 3);
+        let result = eval(display.value);
+        
+        result = Math.round(result * 1000) / 1000; 
+        
+        display.value = result;
+        
+        display.value = eval(display.value);
+      
+
         display.scrollLeft = display.scrollWidth;
     } catch (error) {
         display.value = "Error";
     }
 
-
-    catch (error){
-        display.value = "Error"
-    }
-
-}
-}
-Footer
 }
