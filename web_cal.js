0904a14e73a25d6fb5f352d5c6e70a69d5dc25e7
Sai Arihara Sudan Pasupathy <saiarihara@gmail.com>
2025-01-16 21:32:16 +0530

    Add New Feature of deleting one by one

--- web_cal.js
+++ web_cal.js
@@ -1,22 +1,17 @@
 const display = document.getElementById("display");
 
-
-function appendToDisplay(input){
-
 function appendToDisplay(input) {
     display.value += input;
 }
-
-
-function clearDisplay(){
-
 
 function clearDisplay() {
     display.value = "";
 }
 
+function deleteDisplay() {
+    display.value = display.value.slice(0, -1);
+}
 
-function calculate(){
 function calculate() {
     try {
         let result = eval(display.value);
@@ -26,30 +21,10 @@ function calculate() {
         display.value = result;
 
         display.value = eval(display.value);
-
-
-function calculate() {
-    try {
-        let result = eval(display.value);
-        
-        result = Math.round(result * 1000) / 1000; 
-        
-        display.value = result;
-        
-        display.value = Math.round(eval(display.value), 3);
-      
-
-        display.scrollLeft = display.scrollWidth;
-    } catch (error) {
-        display.value = "Error";
     }
-
-
     catch (error){
         display.value = "Error"
     }
 
 }
-}
-Footer
-}
+
